4bd7ed86fad3fe3eed081b2f35f683a8cc26e3e3
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-08-02 20:44:01 +0600

    array loop table

---
+++ class 03/loops-practice problems/array-loop.js
@@ -0,0 +1,21 @@
+// for(var i = 0; i < 10; i++){
+//     console.log(i);
+// }
+
+//target: display every elements of an array
+// var numbers = [45,87,89,56,32,51,25 ,188,41];
+// for(var i = 3; i < 7; i++){
+//     var number = numbers[i];
+//     console.log(i);
+// }
+// var numbers = [45,87,89,56,32,51,25 ,188,41];
+// for(var i = 0; i < numbers.length; i++){
+//     var number = numbers[i];
+//     console.log(number);
+// }
+
+var items = ['bottle' , 'mouse' , 'sunglass' , 'pen'];
+for(var i =0 ; i < items.length; i++){
+    var item = items[i];
+    console.log(item);
+}
