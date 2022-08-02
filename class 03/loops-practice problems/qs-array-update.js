46131e2bc625fb9db45dde5e45e20cf8d376066e
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-08-02 22:19:32 +0600

    isosceles

---
+++ class 03/loops-practice problems/qs-array-update.js
@@ -0,0 +1,8 @@
+var fruits = ['Apple' , 'Banana' , 'Orange'];
+var bananaIndex = fruits.indexOf('banana');
+fruits[bananaIndex] = 'Mango'
+console.log(fruits);
+fruits.pop();
+console.log(fruits);
+fruits.push('watermalon');
+console.log(fruits);
---
+++ class 03/loops-practice problems/qs-grading.js
@@ -0,0 +1,13 @@
+// var num1 = 13;
+ var num2 = 79;
+var num3 = 45;
+
+if(num1 > num2){
+    if(num1 > num2){
+        console.log(num1);
+    }
+   else{
+      console.log(num3); 
+   }
+}
+ 
