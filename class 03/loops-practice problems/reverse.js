9f27c45e2d561247eefda3b2a25d2cc20a339e3a
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-08-02 21:32:55 +0600

    reverse

---
+++ class 03/loops-practice problems/reverse.js
@@ -0,0 +1,18 @@
+// var number = 0;
+// while(num < 10){
+//     console.log(num);
+//     num++;
+// }
+
+var num = 10;
+while(num > 1){
+    // console.log(num);
+    num--;
+}
+
+for(var i = 1; i<=10; i++){
+    // console.log(i);
+}
+for(var i = 10; i>=1; i--){
+    console.log(i);
+}
