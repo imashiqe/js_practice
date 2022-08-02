ccec18b72bac277f018f9893a07fbab274c57d80
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-08-02 21:06:56 +0600

    break continue

--- class 03/loops-practice problems/for.js
+++ class 03/loops-practice problems/for.js
@@ -19,9 +19,9 @@ for(var roastGiven = 0; roastGiven <7; roastGiven++){
 //     console.log(i);
 // }
 
-for(var i =0; i<=20; i+=2){
-    console.log(i);
-}
-for(var i =1; i<=20; i+=2){
-    console.log(i);
-}
+// for(var i =0; i<=20; i+=2){
+//     console.log(i);
+// }
+// for(var i =1; i<=20; i+=2){
+//     console.log(i);
+// }
