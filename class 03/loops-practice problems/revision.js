5f998cfc8d99ea84a80f77614bfbd97dedea9bef
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-08-02 18:18:15 +0600

    array revision

---
+++ class 03/loops-practice problems/revision.js
@@ -0,0 +1,26 @@
+// string type variable
+var bottle = 'water';
+var bottleColor = 'yellow';
+//number type variable
+var sunglassPrice = 120;
+var penColor = 'blue';
+//------------?........ type variable
+var isExpensive = true;
+
+var tableItems = ['bottle' , 'pen' , 'mouse' , 'sunglass' , 'paper'];
+
+var item3 = tableItems[3];
+tableItems[4] = 'notebook';
+var penIndex = tableItems.indexOf('pen');
+//push pop
+// conditional 
+if (tableItems.length > 4) {
+    
+}
+else if(tableItems[3] == 'pen'){
+
+}
+else{
+    
+}
+
