b259997608131a42deff2aa3a6f75064c32bce4c
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-08-04 18:43:45 +0600

    switch case js

---
+++ class 04/js-function-object/object-looping.js
@@ -0,0 +1,36 @@
+
+
+var shoppingItems = ['books' , 'sunglass' , 'shoes' , 'pen'];
+var friendsAge = [12, 45, 78,12, 32, 14];
+var friendsAge = {
+        rahim: 12,
+        samad: 45,
+        karim: 78,
+        jamal: 12,
+        robi: 32,
+        kazi: 14,
+}
+
+var shoppingCart = {
+    books: 3,
+    sunglass: 1,
+    keyboard: 5,
+    mouse:1,
+    pen: 25
+}
+
+const keys = Object.keys(shoppingCart);
+console.log(keys);
+
+const values = Object.values(shoppingCart);
+console.log(keys);
+
+for(var i =0; i<keys.length; i++){
+    var propertyName = keys[i];
+    var propertyValue = = shoppingCart[propertyName];
+    console.log(propertyName,propertyValue);
+}
+
+// for in loop 
+for(var propertyName in shoppingCart)
+
