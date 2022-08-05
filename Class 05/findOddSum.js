7cfeaa08c44aad62462a0e9d1dbad44b9dafd408
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-08-05 18:41:03 +0600

    oddNumbers Sum

--- Class 05/findOddSum.js
+++ Class 05/findOddSum.js
@@ -4,9 +4,27 @@ function getSumOfAnArray(numbers) {
        const index = i;
        const element = numbers[index];
        sum = sum + element;
-       console.log(index , element);
+    //    console.log(index , element);
    }
 }
 
+function getOddNumbersOfAnArray(numbers){
+        const oddNumbers = [];
+         for(let i = 0; i< numbers.length; i++){
+            const index = i;
+            const element = numbers[index];
+            if (element % 2 !== 1) {
+                console.log(index, element);
+                oddNumbers.push(element);
+            }
+        
+         }
+         return oddNumbers;
+}
+
 const myNumbers = [12, 65, 45, 78, 32, 45, 91];
-getSumOfAnArray(myNumbers);
+const oddNumbers =  getOddNumbersOfAnArray(myNumbers);
+console.log(oddNumbers);
+// getSumOfAnArray(myNumbers);
+const oddNumbersSum = getSumOfAnArray(myNumbers);
+console.log('odd number sum', oddNumbersSum);
