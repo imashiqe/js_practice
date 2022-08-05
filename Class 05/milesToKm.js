7a575744f4ecb13b432c8debf1adbdf1154a8f89
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-08-05 15:09:42 +0600

    miles to kilomiter

---
+++ Class 05/milesToKm.js
@@ -0,0 +1,8 @@
+function milesTokilometer(miles) {
+    const kilometer = miles*1.609;
+    return kilometer;
+
+}
+const kilometer = 12;
+const miles = milesTokilometer(kilometer);
+console.log(miles);
