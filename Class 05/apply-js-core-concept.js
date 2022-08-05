ebc7a67482fb805f3dfeec7beac37ec886d987df
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-08-05 12:48:03 +0600

    recap basic js

---
+++ Class 05/apply-js-core-concept.js
@@ -0,0 +1,52 @@
+//1.variable
+var myName = 'khan';
+
+// math operation: +, -, *, /
+// shorthand: +=, --, *=, /=
+// ++, --
+
+// 2. Array 
+var friends = ['tasin' , 'ashik' , 'mamun' , 'abul'];
+var thirdFriends[2];
+friends[3] = 'dambol';
+
+//3. conditionals
+// >, <, >=, <=, ==, ===, !=, !==
+
+if (friends.length <2) {
+    console.log('fokor tor kono friend nai')
+    
+}
+else{
+
+}
+
+//4. loop 
+var number =0;
+while(number <= 6){
+    console.log(number);
+    number++;
+}
+
+for(var i = 0; i <= 10; i++){
+
+}
+
+//5. function
+function isMoonUp(time) {
+    if (time > 7) {
+        return true;
+    }
+}
+
+// 6. object 
+
+var jantus = {
+    height: 60,
+    romanticism: 'heavy',
+    bappErTaka: 'borolox'
+}
+
+
+
+
