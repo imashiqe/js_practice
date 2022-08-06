4ebb71f57ec1df0001bb4c0e51a9c92a8b226821
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-08-07 00:29:01 +0600

    lowercase and uppercase

---
+++ class 06/js-string-problem-practice/case.js
@@ -0,0 +1,11 @@
+const userName = 'blackPink';
+const userInput = 'blackPink';
+console.log(userName.toLowerCase())
+console.log(userInput.toUpperCase())
+
+if( userName.toLowerCase() === userInput.toLowerCase()){
+    console.log('valid user')
+}
+else{
+    console.log('invalid user');
+}
