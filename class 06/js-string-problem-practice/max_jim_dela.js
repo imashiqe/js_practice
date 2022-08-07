dee5b9e6ae06f47cfad578df62f6a6b53e38b61b
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-08-07 16:05:38 +0600

    max num jim dela solved

---
+++ class 06/js-string-problem-practice/max_jim_dela.js
@@ -0,0 +1,10 @@
+const jim = 69;
+const dela = 97;
+
+if (jim > dela) {
+    console.log('Jim Will get the ckae');
+
+}
+else[
+    console.log('Dela you  Will Get The Cake')
+]
---
+++ class 06/js-string-problem-practice/temp.js
@@ -0,0 +1,15 @@
+let first =5;
+let second = 7;
+console.log(first, second);
+// this is wrong approach
+// first = second;
+// second = first;
+
+//approach: 1
+// const temp = first;
+// first = second;
+// second = temp;
+
+//approach : 2 --- Destructurning
+[first, second] = [second, first];
+console.log(first, second);
