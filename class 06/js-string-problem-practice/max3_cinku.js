79977701e5a406f94edad8550c09b20f60fe0b51
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-08-07 16:23:59 +0600

    max3 cinku problem solved

---
+++ class 06/js-string-problem-practice/max3_cinku.js
@@ -0,0 +1,30 @@
+const jim = 84;
+const dela = 99;
+const chinku = 777;
+
+if (jim > dela && jime > chinku) {
+    console.log('Jim Will get the cake');
+}
+else if( dela > jim && dela > chinku ){
+    console.log('Dela Will Eat cake in this bela');
+}
+else{
+    console.log('Chinku Will get the cake');
+}
+
+//Homework
+// write a function that will take 3 numbers will return the max number
+// write a function that will  take 3 parameters and will return the min number
+
+//first time do it using if-else
+//second time do it using Math.min or Math.max
+
+// take input from the user
+// const num1 = parseFloat(prompt("Enter first number: "));
+// const num2 = parseFloat(prompt("Enter second number: "));
+// const num3 = parseFloat(prompt("Enter third number: "));
+
+// const largest = Math.max(num1, num2, num3);
+
+// // display the result
+// console.log("The largest number is " + largest);
