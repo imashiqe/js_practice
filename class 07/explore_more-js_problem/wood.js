a6238237700b298001722df7d90f03c53b4f1922
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-08-08 14:26:23 +0600

    Wood Calculator totalwood

---
+++ class 07/explore_more-js_problem/wood.js
@@ -0,0 +1,26 @@
+/*
+fixed : per item  wood requerments
+1. chair --> 3 cft
+2. table --> 10 cft
+3. bed --> 50 cft 
+
+vary : quantity 
+*/
+
+function woodCalculator( chairQuantity, tableQuantity, bedQuantity ){
+    const perChairWood = 3;
+    const perTableWood = 10;
+    const perBedWood   = 50;
+    
+    const chairWood = chairQuantity * perChairWood;
+    const tableWood = tableQuantity * perTableWood;
+    const bedWood = bedQuantity * perBedWood;
+
+    // console.log(chairQuantity, tableQuantity, bedQuantity);
+
+    const totalWood = chairWood + tableWood + bedWood;
+    return totalWood;
+}
+
+const totalWood = woodCalculator(1, 0, 0);
+console.log('total wood required: ' , totalWood);
