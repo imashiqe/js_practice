c7f14403a00b8dd02deb3b5427da33351b9f8aba
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-08-08 16:11:45 +0600

    product sum calculation

---
+++ class 07/explore_more-js_problem/simple_cart.js
@@ -0,0 +1,22 @@
+const shoppingCart = [
+    {name: 'shoe', price: 1200},
+    {name: 'shirt', price: 2200},
+    {name: 'pant', price: 3700},
+    {name: 'belt', price: 700},
+];
+
+function totalCost(products){
+    
+let sum = 0;
+    for(let i = 0; i < products.length; i++){
+        const product = products[i];
+        sum = sum + product.price;
+        console.log(product);
+
+    }
+    return sum;
+}
+
+const expense = totalCost(shoppingCart);
+console.log('Total expense today:' , expense);
+
