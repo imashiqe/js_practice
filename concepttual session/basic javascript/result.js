548b0aa9599a4739c86aa8c2c5e5780e6d4e9489
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-08-09 14:53:10 +0600

    added result number

--- concepttual session/basic javascript/result.js
+++ concepttual session/basic javascript/result.js
@@ -1,22 +1,22 @@
-var result = "17";
+var result = "89";
 
 if (result < 33 && result > 0) {
-    console.log("You Are Failed Your Exam")
+    console.log( "Your Mark :", result," You Are Failed Your Exam")
 }
 else if(result >= 33 && result < 40){
-    console.log("Your Grade D");
+    console.log( "Your Mark :", result," Your Grade :  D");
 }
 else if (result >= 40 && result < 50){
-    console.log("Your Grade is C");
+    console.log( "Your Mark :", result," Your Grade is:  C");
 }
 else if (result >= 50 && result < 60){
-    console.log("Your Grade is B");
+    console.log( "Your Mark :", result," Your Grade is : B");
 }
 else if (result >= 60 && result < 79){
-    console.log("Your Grade is A");
+    console.log( "Your Mark :", result," Your Grade is : A");
 }
 else if (result >= 80 && result < 100){
-    console.log("Your Grade is A+");
+    console.log( "Your Mark :", result," Your Grade is:  A+");
 }
 else{
     console.log("Result Not Found Try Again")
